05d88dc2c21be66961b1d1004d7aa531bec1dbf5
myk <michaelmugendi1707@gmail.com>
2024-10-10 11:13:15 +0300

    Update: designed new buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
 // import Image from "next/image";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Button>Primary</Button>
+      <Button variant="secondary">Secondary</Button>
+      <Button variant="destructive">Destructive</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="outline">Outline</Button>
+      <Button variant="ghost">Ghost</Button>
+      <Button variant="teritary">Teritary</Button>
+    </div>
+  );
 }
