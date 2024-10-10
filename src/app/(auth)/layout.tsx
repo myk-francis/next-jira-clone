d3bbd3ab2b45c5432fcf7e944585a9fe930ce06f
myk <michaelmugendi1707@gmail.com>
2024-10-10 13:11:05 +0300

    Update: added logo to navbar

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -7,10 +7,17 @@ interface AuthLayoutProps {
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div>
-      <Image src="/logo.svg" alt="logo" width={100} height={50} />
-      {children}
-    </div>
+    <main className="bg-neutral-100 min-h-screen">
+      {/* <Image src="/logo.svg" alt="logo" width={100} height={50} /> */}
+      <div className="mx-auto max-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <div className="flex items-center gap-2">
+            <Image src="/logo.svg" alt="logo" width={152} height={56} />
+          </div>
+        </nav>
+        {children}
+      </div>
+    </main>
   );
 };
 
