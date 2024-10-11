5e2dc7f432575d580468522824773bf4b029a1ed
myk <michaelmugendi1707@gmail.com>
2024-10-11 11:43:23 +0300

    Add: auth hooks

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -23,8 +23,11 @@ import {
 } from "@/components/ui/form";
 import Link from "next/link";
 import { loginSchema } from "../schemas";
+import { useLogin } from "../api/use-login";
 
 export const SignInCard = () => {
+  const { mutate } = useLogin();
+
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -34,7 +37,7 @@ export const SignInCard = () => {
   });
 
   const onSubmit = (values: z.infer<typeof loginSchema>) => {
-    console.log({ values });
+    mutate({ json: values });
   };
 
   return (
--- src/features/auth/schemas.ts
+++ src/features/auth/schemas.ts
@@ -4,3 +4,9 @@ export const loginSchema = z.object({
   email: z.string().email(),
   password: z.string().min(1, "Required").max(256),
 });
+
+export const registerSchema = z.object({
+  name: z.string().trim().min(1, "Required").max(256),
+  email: z.string().email(),
+  password: z.string().min(1, "Required").max(256),
+});
