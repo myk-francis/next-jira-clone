c5b8bce1a2f34f3772ca47972883ce3355e0edb9
myk <michaelmugendi1707@gmail.com>
2024-10-13 14:59:37 +0300

    Update: toast message

--- src/features/auth/api/use-register.ts
+++ src/features/auth/api/use-register.ts
@@ -2,6 +2,7 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 import { client } from "@/lib/rpc";
 import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 type ResponseType = InferResponseType<
   (typeof client.api.auth.register)["$post"]
@@ -17,10 +18,14 @@ export const useRegister = () => {
       return await response.json();
     },
     onSuccess: () => {
+      toast.success("Registered");
       router.refresh();
       queryClient.invalidateQueries({
         queryKey: ["current"],
       });
+    },
+    onError: () => {
+      toast.error("Failed to register");
     },
   });
 
--- src/features/workspaces/api/use-create-workspace.ts
+++ src/features/workspaces/api/use-create-workspace.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 import { client } from "@/lib/rpc";
+import { toast } from "sonner";
 
 type ResponseType = InferResponseType<(typeof client.api.workspaces)["$post"]>;
 type RequestType = InferRequestType<(typeof client.api.workspaces)["$post"]>;
@@ -13,9 +14,13 @@ export const useCreateWorkspace = () => {
       return await response.json();
     },
     onSuccess: () => {
+      toast.success("Workspace created");
       queryClient.invalidateQueries({
         queryKey: ["workspaces"],
       });
+    },
+    onError: () => {
+      toast.error("Failed to create workspace");
     },
   });
 
