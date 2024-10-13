b31806277ac6ecf30c38f2178f7deb843121aca3
myk <michaelmugendi1707@gmail.com>
2024-10-13 10:29:51 +0300

    Update: actions.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,16 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { useCurrent } from "@/features/auth/api/use-current";
-import { useLogout } from "@/features/auth/api/use-logout";
+// import { Button } from "@/components/ui/button";
+// import { useCurrent } from "@/features/auth/api/use-current";
+import { getCurrent } from "@/features/auth/actions";
 import { UserButton } from "@/features/auth/components/user-button";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import React from "react";
 
-export default function Home() {
-  const router = useRouter();
-
-  const { data, isLoading } = useCurrent();
-  const { mutate } = useLogout();
-
-  React.useEffect(() => {
-    if (!data && !isLoading) {
-      router.push("/sign-in");
-    }
-  }, [data, isLoading, router]);
+export default async function Home() {
+  const user = await getCurrent();
+  if (!user) {
+    redirect("/sign-in");
+  }
 
   return (
     <div>
