a970c1aa1cb4237e4829fad486722787ceb4d48d
myk <michaelmugendi1707@gmail.com>
2024-10-13 13:01:02 +0300

    Update: mobile-sidebar.tsx finished

--- src/components/mobile-sidebar.tsx
+++ src/components/mobile-sidebar.tsx
@@ -1,17 +1,30 @@
 "use client";
 import React from "react";
-import { Sheet, SheetTrigger } from "./ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import { Sidebar } from "./sidebar";
+import { usePathname } from "next/navigation";
 
 export const MobileSidebar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const pathname = usePathname();
+
+  React.useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
   return (
-    <Sheet>
+    <Sheet modal={false} open={isOpen} onOpenChange={setIsOpen}>
       <SheetTrigger asChild>
-        <Button variant={"secondary"} className="lg:hidden ">
-          <MenuIcon className="h-6 w-6" />
+        <Button size={"icon"} variant={"secondary"} className="lg:hidden ">
+          <MenuIcon className="size-4 text-neutral-500" />
         </Button>
       </SheetTrigger>
+      <SheetContent side={"left"} className="p-0">
+        {/* <SheetTitle className="hidden lg:block">Navigation</SheetTitle> */}
+        <Sidebar />
+      </SheetContent>
     </Sheet>
   );
 };
