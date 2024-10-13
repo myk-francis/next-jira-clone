b2e3cb765cb86f6474ceb69c31143b125d65fc33
myk <michaelmugendi1707@gmail.com>
2024-10-13 20:40:21 +0300

    Update: nuq page as state

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -20,12 +20,14 @@ import { useCreateWorkspace } from "../api/use-create-workspace";
 import Image from "next/image";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import { ImageIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface CreateWorkspaceFormProps {
   onCancel?: () => void;
 }
 
 const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
+  const router = useRouter();
   const { mutate, isPending } = useCreateWorkspace();
 
   const inputRef = React.useRef<HTMLInputElement>(null);
@@ -46,8 +48,10 @@ const CreateWorkspaceForm = ({ onCancel }: CreateWorkspaceFormProps) => {
     mutate(
       { form: finalValues },
       {
-        onSuccess: () => form.reset(),
-        //TODO: Redirect to new workspace
+        onSuccess: ({ data }) => {
+          form.reset();
+          router.push(`/workspaces/${data.$id}`);
+        },
       }
     );
   };
--- src/features/workspaces/components/create-workspace-modal.tsx
+++ src/features/workspaces/components/create-workspace-modal.tsx
@@ -1,11 +1,13 @@
 "use client";
 import ResposnsiveModal from "@/components/responsive-modal";
 import CreateWorkspaceForm from "./create-workspace-form";
+import { useCreateWorkspaceModal } from "../hooks/use-create-workspace-modal";
 
 export const CreateWorkspaceModal = () => {
+  const { isOpen, setIsOpen, close } = useCreateWorkspaceModal();
   return (
-    <ResposnsiveModal open onOpenChange={() => {}}>
-      <CreateWorkspaceForm />
+    <ResposnsiveModal open={isOpen} onOpenChange={setIsOpen}>
+      <CreateWorkspaceForm onCancel={close} />
     </ResposnsiveModal>
   );
 };
