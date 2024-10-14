3c2eb33c26d550ba301e8a151242983d9518a1ae
myk <michaelmugendi1707@gmail.com>
2024-10-14 08:56:11 +0300

    Update: update workspace

--- src/features/workspaces/schemas.ts
+++ src/features/workspaces/schemas.ts
@@ -9,3 +9,18 @@ export const createWorkspaceSchema = z.object({
     ])
     .optional(),
 });
+
+export const updateWorkspaceSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(1, "Must be 1 or more characters")
+    .max(256)
+    .optional(),
+  image: z
+    .union([
+      z.instanceof(File),
+      z.string().transform((value) => (value === "" ? undefined : value)),
+    ])
+    .optional(),
+});
