d59d1a6579919cfb960593eb6df24d108a557158
myk <michaelmugendi1707@gmail.com>
2024-10-15 10:08:44 +0300

    Update: join-workspace-form

--- src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -1,6 +1,21 @@
+import { getCurrent } from "@/features/auth/queries";
+import { redirect } from "next/navigation";
 import React from "react";
 
-const WorkspaceIdJoinPage = () => {
+interface WorkspaceIdJoinPageProps {
+  params: {
+    workspaceId: string;
+  };
+}
+
+const WorkspaceIdJoinPage = async ({
+  params: { workspaceId },
+}: WorkspaceIdJoinPageProps) => {
+  const user = await getCurrent();
+  if (!user) {
+    redirect("/sign-in");
+  }
+
   return <div>WorkspaceIdJoinPage</div>;
 };
 
---
+++ src/features/workspaces/components/join-workspace-form.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const JoinWorkspaceForm = () => {
+  return <div>JoinWorkspaceForm</div>;
+};
