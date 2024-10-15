509072c61d896a98dbdb319a9d9b214c1a4299a5
myk <michaelmugendi1707@gmail.com>
2024-10-15 12:54:10 +0300

    Update: finished members list

--- src/features/members/server/route.ts
+++ src/features/members/server/route.ts
@@ -47,7 +47,7 @@ const app = new Hono()
       return c.json({
         data: {
           ...members,
-          doucments: populatedMembers,
+          documents: populatedMembers,
         },
       });
     }
--- src/features/workspaces/components/members-list.tsx
+++ src/features/workspaces/components/members-list.tsx
@@ -1,25 +1,63 @@
 "use client";
 import React, { Fragment } from "react";
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DottedSeparator } from "@/components/dotted-separator";
 import { useWorkspaceId } from "../hooks/use-workspace-id";
-import { ArrowLeftIcon } from "lucide-react";
+import { ArrowLeftIcon, MoreVertical } from "lucide-react";
 import Link from "next/link";
 import { useGetMembers } from "@/features/members/api/use-get-members";
+import { MemberAvatar } from "@/features/members/components/member-avatar";
+import { Separator } from "@/components/ui/separator";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useDeleteMember } from "@/features/members/api/use-delete-member";
+import { useUpdateMember } from "@/features/members/api/use-update-member";
+import { MemberRole } from "@/features/members/types";
+import { useConfirm } from "@/hooks/use-confirm";
 
 export const MembersList = () => {
   const workspaceId = useWorkspaceId();
-  const { data } = useGetMembers(workspaceId);
+  const { data } = useGetMembers({ workspaceId });
+  const { mutate: deleteMember, isPending: isDeletingMember } =
+    useDeleteMember();
+  const { mutate: updateMember, isPending: isUpdatingMember } =
+    useUpdateMember();
+  const [confirmRemove, ConfirmDialog] = useConfirm(
+    "Remove Member",
+    "This member will be removed from the workspace",
+    "destructive"
+  );
+
+  const handleUpdateMember = (memberId: string, role: MemberRole) => {
+    updateMember({
+      param: { memberId },
+      json: { role },
+    });
+  };
+
+  const handleDeleteMember = async (memberId: string) => {
+    const ok = await confirmRemove();
+    if (!ok) return;
+    deleteMember(
+      {
+        param: { memberId },
+      },
+      {
+        onSuccess: () => {
+          window.location.reload();
+        },
+      }
+    );
+  };
 
   return (
     <Card className="w-full h-full border-none shadow-none">
+      <ConfirmDialog />
       <CardHeader className="flex flex-row items-center gap-x-4 p-7 space-y-0">
         <Button asChild variant="secondary" size={"sm"} onClick={() => {}}>
           <Link href={`/workspaces/${workspaceId}`}>
@@ -34,9 +72,60 @@ export const MembersList = () => {
       </div>
 
       <CardContent className="p-7">
-        {data?.documents.map((member) => (
+        {data?.documents.map((member, index) => (
           <Fragment key={member.$id}>
-            <div className="flex items-center gap-2"></div>
+            <div className="flex items-center gap-2">
+              <MemberAvatar
+                name={member.name}
+                className="size-10"
+                fallbackClassName="text-lg"
+              />
+              <div className="flex flex-col">
+                <p className="text-sm font-medium">{member.name}</p>
+                <p className="text-xs text-muted-foreground">{member.email}</p>
+              </div>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant={"secondary"}
+                    size={"icon"}
+                    className="ml-auto"
+                  >
+                    <MoreVertical className="size-4 text-muted-foreground" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent side="bottom" align="end">
+                  <DropdownMenuItem
+                    onClick={() =>
+                      handleUpdateMember(member.$id, MemberRole.ADMIN)
+                    }
+                    className="font-medium"
+                    disabled={isUpdatingMember}
+                  >
+                    Set as admin
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    onClick={() =>
+                      handleUpdateMember(member.$id, MemberRole.MEMBER)
+                    }
+                    className="font-medium"
+                    disabled={isUpdatingMember}
+                  >
+                    Set as member
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    onClick={() => handleDeleteMember(member.$id)}
+                    className="font-medium"
+                    disabled={isDeletingMember}
+                  >
+                    Remove {member.name}
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+            {index < data.documents.length - 1 && (
+              <Separator className="my-2.5" />
+            )}
           </Fragment>
         ))}
       </CardContent>
