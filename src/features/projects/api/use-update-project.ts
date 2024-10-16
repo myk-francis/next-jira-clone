0cb56ba6262b1160e5a821fbd09b8a328417937f
myk <michaelmugendi1707@gmail.com>
2024-10-16 16:33:12 +0300

    Update: use-update-project

---
+++ src/features/projects/api/use-update-project.ts
@@ -0,0 +1,39 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { InferRequestType, InferResponseType } from "hono";
+import { client } from "@/lib/rpc";
+import { toast } from "sonner";
+
+type ResponseType = InferResponseType<
+  (typeof client.api.projects)[":projectId"]["$patch"],
+  200
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.projects)[":projectId"]["$patch"]
+>;
+
+export const useUpdateProject = () => {
+  const queryClient = useQueryClient();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async ({ form, param }) => {
+      const response = await client.api.projects[":projectId"]["$patch"]({
+        form,
+        param,
+      });
+      if (!response.ok) {
+        throw new Error("Failed to update project");
+      }
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast.success("Project updated");
+      queryClient.invalidateQueries({
+        queryKey: ["projects"],
+      });
+    },
+    onError: () => {
+      toast.error("Project to update workspace");
+    },
+  });
+
+  return mutation;
+};
--- src/features/projects/schemas.ts
+++ src/features/projects/schemas.ts
@@ -10,3 +10,13 @@ export const createProjectSchema = z.object({
     ])
     .optional(),
 });
+
+export const updateProjectSchema = z.object({
+  name: z.string().trim().min(1, "Minimum 1 character").max(256).optional(),
+  image: z
+    .union([
+      z.instanceof(File),
+      z.string().transform((value) => (value === "" ? undefined : value)),
+    ])
+    .optional(),
+});
