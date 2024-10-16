7a313f1bb8877fbd81127186b2e2133b3e8f7e66
myk <michaelmugendi1707@gmail.com>
2024-10-16 13:33:03 +0300

    Update: schema

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -1,37 +1,93 @@
-import { DATABASE_ID, PROJECTS_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, PROJECTS_ID } from "@/config";
 import { getMember } from "@/features/members/utils";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
-import { Query } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { z } from "zod";
+import { createProjectSchema } from "../schema";
 
-const app = new Hono().get(
-  "/",
-  sessionMiddleware,
-  zValidator("query", z.object({ workspaceId: z.string() })),
-  async (c) => {
-    const databases = c.get("databases");
-    const user = c.get("user");
-    const { workspaceId } = c.req.query();
+const app = new Hono()
+  .get(
+    "/",
+    sessionMiddleware,
+    zValidator("query", z.object({ workspaceId: z.string() })),
+    async (c) => {
+      const databases = c.get("databases");
+      const user = c.get("user");
+      const { workspaceId } = c.req.query();
 
-    const member = await getMember({
-      databases,
-      workspaceId,
-      userId: user.$id,
-    });
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
 
-    if (!member) {
-      return c.json({ error: "Unauthorized" }, 401);
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      const projects = await databases.listDocuments(DATABASE_ID, PROJECTS_ID, [
+        Query.equal("workspaceId", workspaceId),
+        Query.orderDesc("$createdAt"),
+      ]);
+
+      return c.json({ data: projects });
     }
+  )
+  .post(
+    "/",
+    zValidator("form", createProjectSchema),
+    sessionMiddleware,
+    async (c) => {
+      const databases = c.get("databases");
+      const storage = c.get("storage");
+      const user = c.get("user");
+      const { name, image, workspaceId } = c.req.valid("form");
 
-    const projects = await databases.listDocuments(DATABASE_ID, PROJECTS_ID, [
-      Query.equal("workspaceId", workspaceId),
-      Query.orderDesc("$createdAt"),
-    ]);
+      const member = await getMember({
+        databases,
+        workspaceId,
+        userId: user.$id,
+      });
 
-    return c.json({ data: projects });
-  }
-);
+      if (!member) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
+
+      let uploadedImageUrl: string | undefined;
+
+      if (image instanceof File) {
+        const file = await storage.createFile(
+          IMAGES_BUCKET_ID,
+          ID.unique(),
+          image
+        );
+
+        const arrayBuffer = await storage.getFilePreview(
+          IMAGES_BUCKET_ID,
+          file.$id
+        );
+
+        uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+          arrayBuffer
+        ).toString("base64")}`;
+      }
+
+      const project = await databases.createDocument(
+        DATABASE_ID,
+        PROJECTS_ID,
+        ID.unique(),
+        {
+          name,
+          userId: user.$id,
+          workspaceId: workspaceId,
+          imageUrl: uploadedImageUrl,
+        }
+      );
+
+      return c.json({ data: project });
+    }
+  );
 
 export default app;
