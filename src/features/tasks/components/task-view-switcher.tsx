f60f3b6b57869bf39f12bf803e01afa1579cc1f3
myk <michaelmugendi1707@gmail.com>
2024-10-17 09:13:32 +0300

    Update: schemas

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -1,5 +1,45 @@
 import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { DottedSeparator } from "@/components/dotted-separator";
 
 export const TaskViewSwitcher = () => {
-  return <div>TaskViewSwitcher</div>;
+  return (
+    <Tabs className="flex-1 w-full border rounded-lg">
+      <div className="h-full flex flex-col overflow-auto p-4 ">
+        <div className="flex flex-col gap-y-2 lg:flex-row justify-between">
+          <TabsList className="w-full lg:w-auto">
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="table">
+              Table
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="kanban">
+              kanban
+            </TabsTrigger>
+            <TabsTrigger className="h-8 w-full lg:w-auto" value="calender">
+              Calender
+            </TabsTrigger>
+          </TabsList>
+          <Button className="w-full lg:w-auto">
+            <PlusIcon className="size-4 mr-2" />
+            New
+          </Button>
+        </div>
+        <DottedSeparator className="my-4" />
+        Data filters
+        <DottedSeparator className="my-4" />
+        <>
+          <TabsContent value="table" className="mt-0">
+            Data table
+          </TabsContent>
+          <TabsContent value="kanban" className="mt-0">
+            Data kanban
+          </TabsContent>
+          <TabsContent value="calender" className="mt-0">
+            Data calender
+          </TabsContent>
+        </>
+      </div>
+    </Tabs>
+  );
 };
