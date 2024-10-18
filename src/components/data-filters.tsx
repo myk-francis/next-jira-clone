34fd12cb7b8d82e1e7041fd0a984ed188afad740
myk <michaelmugendi1707@gmail.com>
2024-10-18 14:12:13 +0300

    Update: use-task-filters

--- src/components/data-filters.tsx
+++ src/components/data-filters.tsx
@@ -1,5 +1,66 @@
+import { useGetMembers } from "@/features/members/api/use-get-members";
+import { useGetProjects } from "@/features/projects/api/use-get-projects";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import React from "react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectSeparator,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import { ListCheckIcon } from "lucide-react";
+import { TaskStatus } from "@/features/tasks/types";
 
-export const DataFilters = () => {
-  return <div>DataFilters</div>;
+interface DataFiltersProps {
+  hideProjectFilters?: boolean;
+}
+
+export const DataFilters = ({ hideProjectFilters }: DataFiltersProps) => {
+  const workspaceId = useWorkspaceId();
+  const { data: projects, isLoading: isLoadingProjects } = useGetProjects({
+    workspaceId,
+  });
+  const { data: members, isLoading: isLoadingMembers } = useGetMembers({
+    workspaceId,
+  });
+
+  const isLoading = isLoadingProjects || isLoadingMembers;
+
+  const projectOptions = projects?.documents.map((project) => ({
+    value: project.$id,
+    label: project.name,
+  }));
+
+  const memberOptions = members?.documents.map((member) => ({
+    value: member.$id,
+    label: member.name,
+  }));
+
+  if (isLoading) {
+    return null;
+  }
+
+  return (
+    <div className="flex flex-col lg:flex-row gap-2">
+      <Select defaultValue={undefined} onValueChange={() => {}}>
+        <SelectTrigger className="w-full lg:w-auto h-8">
+          <div className="flex items-center pr-2">
+            <ListCheckIcon className="size-4 mr-2" />
+            <SelectValue placeholder="All statuses" />
+          </div>
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="all">All statuses</SelectItem>
+          <SelectSeparator />
+          <SelectItem value={TaskStatus.BACKLOG}>Backlog</SelectItem>
+          <SelectItem value={TaskStatus.IN_PROGRESS}>In Progress</SelectItem>
+          <SelectItem value={TaskStatus.IN_REVIEW}>In Review</SelectItem>
+          <SelectItem value={TaskStatus.TODO}>Todo</SelectItem>
+          <SelectItem value={TaskStatus.DONE}>Done</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
 };
--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -8,6 +8,7 @@ import { useCreateTaskModal } from "../hooks/use-create-task-modal";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 import { useGetTasks } from "../api/use-get-tasks";
 import { useQueryState } from "nuqs";
+import { DataFilters } from "@/components/data-filters";
 
 export const TaskViewSwitcher = () => {
   const [view, setView] = useQueryState("task-view", {
@@ -43,7 +44,7 @@ export const TaskViewSwitcher = () => {
           </Button>
         </div>
         <DottedSeparator className="my-4" />
-        Data filters
+        <DataFilters />
         <DottedSeparator className="my-4" />
         {isLoadingTasks ? (
           <div className="w-full border rounded-lg h-[200px] flex flex-col items-center justify-center">
