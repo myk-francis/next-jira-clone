289846bec9860f29fbd58705ad5401cdaf13d7c6
myk <michaelmugendi1707@gmail.com>
2024-10-18 16:56:30 +0300

    Update: task-actions almost

--- src/features/tasks/components/columns.tsx
+++ src/features/tasks/components/columns.tsx
@@ -2,13 +2,14 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import { Task } from "../types";
-import { ArrowUpDown } from "lucide-react";
+import { ArrowUpDown, MoreVerticalIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { MemberAvatar } from "@/features/members/components/member-avatar";
 import { TaskDate } from "./task-date";
 import { Badge } from "@/components/ui/badge";
 import { snakeCaseToTitleCase } from "@/lib/utils";
+import { TaskActions } from "./task-actions";
 
 export const columns: ColumnDef<Task>[] = [
   {
@@ -117,6 +118,20 @@ export const columns: ColumnDef<Task>[] = [
     cell: ({ row }) => {
       const status = row.original.status;
       return <Badge variant={status}>{snakeCaseToTitleCase(status)}</Badge>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const id = row.original.id;
+      const projectId = row.original.projectId;
+      return (
+        <TaskActions id={id} projectId={projectId}>
+          <Button variant={"ghost"} className="size-8 p-0">
+            <MoreVerticalIcon className="size-4" />
+          </Button>
+        </TaskActions>
+      );
     },
   },
 ];
--- src/features/tasks/components/task-actions.tsx
+++ src/features/tasks/components/task-actions.tsx
@@ -1,5 +1,59 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ExternalLinkIcon, PencilIcon, TrashIcon } from "lucide-react";
 import React from "react";
 
-export const TaskActions = () => {
-  return <div>TaskActions</div>;
+interface TaskActionsProps {
+  id: string;
+  projectId: string;
+  children?: React.ReactNode;
+}
+
+export const TaskActions = ({ id, projectId, children }: TaskActionsProps) => {
+  return (
+    <div className="flex justify-end">
+      <DropdownMenu modal={false}>
+        <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuItem
+            className="font-medium p-[10px]"
+            onClick={() => {}}
+            disabled={false}
+          >
+            <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
+            Task Details
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="font-medium p-[10px]"
+            onClick={() => {}}
+            disabled={false}
+          >
+            <ExternalLinkIcon className="size-4 mr-2 stroke-2" />
+            Open Project
+          </DropdownMenuItem>
+          <DropdownMenuItem
+            className="font-medium p-[10px]"
+            onClick={() => {}}
+            disabled={false}
+          >
+            <PencilIcon className="size-4 mr-2 stroke-2" />
+            Edit Task
+          </DropdownMenuItem>
+
+          <DropdownMenuItem
+            className="text-amber-700 focus:text-amber-700 font-medium p-[10px]"
+            onClick={() => {}}
+            disabled={false}
+          >
+            <TrashIcon className="size-4 mr-2 stroke-2" />
+            Delete Task
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
 };
