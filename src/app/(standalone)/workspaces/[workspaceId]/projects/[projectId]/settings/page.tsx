ac65c5d137271c7ff427bdcbd789c1b2a3aff45b
myk <michaelmugendi1707@gmail.com>
2024-10-21 21:30:20 +0300

    Update: clients page

--- src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/projects/[projectId]/settings/page.tsx
@@ -1,35 +1,15 @@
 import { getCurrent } from "@/features/auth/queries";
-import EditProjectForm from "@/features/projects/components/edit-project-form";
-import { getProject } from "@/features/projects/queries";
 import { redirect } from "next/navigation";
 import React from "react";
+import ProjectIdSettingsClient from "./client";
 
-interface ProjectIdSettingsPageProps {
-  params: {
-    workspaceId: string;
-    projectId: string;
-  };
-}
-
-const ProjectIdSettingsPage = async ({
-  params: { projectId, workspaceId },
-}: ProjectIdSettingsPageProps) => {
+const ProjectIdSettingsPage = async () => {
   const user = await getCurrent();
   if (!user) {
     redirect("/sign-in");
   }
 
-  const initialValues = await getProject({ projectId });
-
-  if (!initialValues) {
-    redirect(`/workspaces/${workspaceId}`);
-  }
-
-  return (
-    <div className="w-full lg:max-w-xl">
-      <EditProjectForm initialValues={initialValues} />
-    </div>
-  );
+  return <ProjectIdSettingsClient />;
 };
 
 export default ProjectIdSettingsPage;
