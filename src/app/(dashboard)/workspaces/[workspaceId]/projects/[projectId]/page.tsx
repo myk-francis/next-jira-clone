97cfb20c37bac441088fdce6b06530a2c8264435
myk <michaelmugendi1707@gmail.com>
2024-10-21 21:06:00 +0300

    Update: use-project-id

--- src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/projects/[projectId]/page.tsx
@@ -1,20 +1,10 @@
-import { Button } from "@/components/ui/button";
 import { getCurrent } from "@/features/auth/queries";
-import { ProjectAvatar } from "@/features/projects/components/project-avatar";
 import { getProject } from "@/features/projects/queries";
-import { TaskViewSwitcher } from "@/features/tasks/components/task-view-switcher";
-import { PencilIcon } from "lucide-react";
-import Link from "next/link";
 import { redirect } from "next/navigation";
 import React from "react";
+import ProjectIdClient from "./client";
 
-interface ProjectIdPageProps {
-  params: {
-    projectId: string;
-  };
-}
-
-const ProjectIdPage = async ({ params: { projectId } }: ProjectIdPageProps) => {
+const ProjectIdPage = async () => {
   const user = await getCurrent();
   if (!user) {
     redirect("/sign-in");
@@ -26,33 +16,7 @@ const ProjectIdPage = async ({ params: { projectId } }: ProjectIdPageProps) => {
     throw new Error("Project not found");
   }
 
-  return (
-    <div className="flex flex-col gap-y-4">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center gap-x-2">
-          <ProjectAvatar
-            name={initialValues.name}
-            image={initialValues.imageUrl}
-            className="size-8"
-          />
-          <p className="text-lg font-semibold">{initialValues.name}</p>
-        </div>
-        <div>
-          <Button className="" variant={"secondary"} size={"sm"} asChild>
-            <Link
-              href={`/workspaces/${initialValues.workspaceId}/projects/${initialValues.$id}/settings`}
-            >
-              <PencilIcon className="size-4 mr-2" />
-              Edit Project
-            </Link>
-          </Button>
-        </div>
-      </div>
-      <div>
-        <TaskViewSwitcher hideProjectFilters />
-      </div>
-    </div>
-  );
+  return <ProjectIdClient />;
 };
 
 export default ProjectIdPage;
