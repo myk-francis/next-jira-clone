51da1ff921401543edf89757363592e856fd37ba
myk <michaelmugendi1707@gmail.com>
2024-10-21 19:31:31 +0300

    Update: page-loader finished

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,3 +1,4 @@
+import { PageLoader } from "@/components/page-loader";
 import { useGetTask } from "@/features/tasks/api/use-get-task";
 import { useTaskId } from "@/features/tasks/hooks/use-task-id";
 import React from "react";
@@ -5,5 +6,9 @@ import React from "react";
 export const TaskIdClient = () => {
   const taskId = useTaskId();
   const { data, isLoading } = useGetTask();
+
+  if (isLoading) {
+    return <PageLoader />;
+  }
   return <div>TaskIdClient</div>;
 };
--- src/components/page-loader.tsx
+++ src/components/page-loader.tsx
@@ -1,5 +1,10 @@
+import { Loader } from "lucide-react";
 import React from "react";
 
 export const PageLoader = () => {
-  return <div>PageLoader</div>;
+  return (
+    <div className="h-full flex items-center justify-center">
+      <Loader className="size-6 animate-spin text-muted-foreground" />
+    </div>
+  );
 };
