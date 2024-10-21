b80c4698b1c88cdd734013112ca2028326325893
myk <michaelmugendi1707@gmail.com>
2024-10-21 20:49:06 +0300

    Update: TaskDescription finished

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -4,6 +4,7 @@ import PageError from "@/components/page-error";
 import { PageLoader } from "@/components/page-loader";
 import { useGetTask } from "@/features/tasks/api/use-get-task";
 import { TaskBreadcrumbs } from "@/features/tasks/components/task-breadcrumbs";
+import { TaskDescription } from "@/features/tasks/components/task-description";
 import { TaskOverview } from "@/features/tasks/components/task-overview";
 import { useTaskId } from "@/features/tasks/hooks/use-task-id";
 import React from "react";
@@ -25,6 +26,7 @@ export const TaskIdClient = () => {
       <DottedSeparator className="my-6" />
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <TaskOverview task={data} />
+        <TaskDescription task={data} />
       </div>
     </div>
   );
