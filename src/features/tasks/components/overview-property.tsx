b6222e983be4a428a4a6200ae492acc5d3d76f47
myk <michaelmugendi1707@gmail.com>
2024-10-21 20:13:03 +0300

    Update: overview-property

---
+++ src/features/tasks/components/overview-property.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const OverviewProperty = () => {
+  return <div>OverviewProperty</div>;
+};
--- src/features/tasks/components/task-overview.tsx
+++ src/features/tasks/components/task-overview.tsx
@@ -1,10 +1,26 @@
 import React from "react";
 import { Task } from "../types";
+import { Button } from "@/components/ui/button";
+import { PencilIcon } from "lucide-react";
+import { DottedSeparator } from "@/components/dotted-separator";
 
 interface TaskOverviewProps {
   task: Task;
 }
 
 export const TaskOverview = ({ task }: TaskOverviewProps) => {
-  return <div>TaskOverview</div>;
+  return (
+    <div className="flex flex-col gap-y-4 col-span-1">
+      <div className="bg-muted rounded-lg p-4">
+        <div className="flex items-center justify-between">
+          <p className="text-lg font-semibold">Overview</p>
+          <Button size={"sm"} variant={"secondary"}>
+            <PencilIcon className="size-4 mr-2" />
+          </Button>
+        </div>
+        <DottedSeparator className="my-4" />
+        <div className="flex flex-col gap-y-4"></div>
+      </div>
+    </div>
+  );
 };
