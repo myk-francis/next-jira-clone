6597b704c57fdd08f671a50fb3b1484609b3b05a
myk <michaelmugendi1707@gmail.com>
2024-10-21 21:16:34 +0300

    Update: use-project-id hook

--- src/features/projects/server/route.ts
+++ src/features/projects/server/route.ts
@@ -6,6 +6,7 @@ import { Hono } from "hono";
 import { ID, Query } from "node-appwrite";
 import { z } from "zod";
 import { createProjectSchema, updateProjectSchema } from "../schemas";
+import { Project } from "../types";
 
 const app = new Hono()
   .get(
@@ -35,6 +36,29 @@ const app = new Hono()
       return c.json({ data: projects });
     }
   )
+  .get("/:projectId", sessionMiddleware, async (c) => {
+    const databases = c.get("databases");
+    const user = c.get("user");
+    const { projectId } = c.req.query();
+
+    const project = await databases.getDocument<Project>(
+      DATABASE_ID,
+      PROJECTS_ID,
+      projectId
+    );
+
+    const member = await getMember({
+      databases,
+      workspaceId: project.workspaceId,
+      userId: user.$id,
+    });
+
+    if (!member) {
+      return c.json({ error: "Unauthorized" }, 401);
+    }
+
+    return c.json({ data: project });
+  })
   .post(
     "/",
     zValidator("form", createProjectSchema),
