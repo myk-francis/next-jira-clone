b03f91e986559e8238ae584eee711dc363c5a791
myk <michaelmugendi1707@gmail.com>
2024-10-21 19:30:16 +0300

    Update: page-loader

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,5 +1,9 @@
+import { useGetTask } from "@/features/tasks/api/use-get-task";
+import { useTaskId } from "@/features/tasks/hooks/use-task-id";
 import React from "react";
 
 export const TaskIdClient = () => {
+  const taskId = useTaskId();
+  const { data, isLoading } = useGetTask();
   return <div>TaskIdClient</div>;
 };
